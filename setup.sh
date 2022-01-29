436622a912c38a0b59b025ff1a44b2a34b844028
Ephron Martin <94764266+EphronM@users.noreply.github.com>
2022-01-29 15:58:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-[theme]\n\
-primaryColor="#ECE5DD"\n\
-backgroundColor="#FFFFFF"\n\
-secondaryBackgroundColor="#128C7E"\n\
-textColor="#262730"\n\
-font = "sans serif"\n\
-
 " > ~/.streamlit/config.toml
