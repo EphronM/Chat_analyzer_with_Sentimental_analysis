aeb0f65eb5eafedd9de8a43256dd82685cc1b38f
Ephron Martin <94764266+EphronM@users.noreply.github.com>
2022-01-29 19:34:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
+mkdir -p ~/.streamlit/
+echo "[theme]
+primaryColor=’#020202’
+backgroundColor=’#c4c3c3’
+secondaryBackgroundColor=’#ebd316’
+font = ‘sans serif’
+[server]
 headless = true
 port = $PORT
 enableCORS = false
