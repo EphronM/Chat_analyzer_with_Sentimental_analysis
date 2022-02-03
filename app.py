b7cc161cf3bcc9b446c03f68f5a033c86bf32f53
Ephron Martin <94764266+EphronM@users.noreply.github.com>
2022-02-03 20:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,26 @@ from preprocessor import preprocess_data
 import analyzer
 import seaborn as sns
 import matplotlib.pyplot as plt
+import base64
 
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('chat_analyzer_bg.jpg')
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 uploaded_file = st.sidebar.file_uploader('Choose a file')
